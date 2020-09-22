3fd19c5de5710651d6c81e3bb82a9814a1119ca8
ShiKhu <alex.kaminsky.11@gmail.com>
2020-09-22 14:05:56 +0200

    Task 2 explained

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Application Task for Insai-Tech
+
+Work is still in progres and might be complemented over the time till the deadline during moments of inspiration...
+
+## Task 1
+Information in comments
+
+
+## Task 2
+let R be the conflict radius. 
+L - a size of a bundle.
+N - neurons count
+
+Task can be solved, for example, by sorting neurons by one coordinate ( O(NlogN) ) then checking for closest neighbours till the x + R coordinate. Which will be average O(N* L/R) with the uniform distribution of neurons.
+Yet, this check might take near the O(N^2) in the worst case, when all neurons are close eonugh in given coordinate, but rearly conflict in another.
+Indexing by both coordinates might improve the situation, but makes it hard to select neighbours on both dimentions simultaneously and also might affect complexity on a specific cases.
+
+Also we could think about using descrete nature of the coordinates and mark "conflict area". 
+This might work if we have R^2 << N or/and hardware that performs fast on matrix operations, such as masking. Which is not our case here.
+
+Implemented soulution uses grid cells with the diagonal of R that considered as a potential conflict area. (cell dimention d = ⌊R/√2⌋)
+Thus, two neurons in one cell are going to be in conflict by definition. Also we must consider that neuron will affect surrounding cells (8 cells).
+Starting to read neurons one by one, we add information about their positions on the grid. If the cell contains at least one another neuron, we mark the new one as conflicting (and the old one too if it is alone). Also we should check all the surrounding cells that might be affected (count C of such sells is < 16, cause R < √2*(d+1) ~ √2d as d >> 1 and the max cells count intersected with area is when neuron is in the corner of cell ) for non-conflicting neurons at the moment (they have max 1 in a cell, otherwise - they are already marked as conflicting). The worst case is when we have new neuron in a cell and many neurons around to check. But we can't have big amount of such "unlucky" neurons. If one such neuron has k neighbors to check, that means that k-C of them are not first in their cell and was checked by O(1). Thus, increase in complexity of checking one neuron to O(k), brings us asymptotycally same number of fast-checked neurons. (k-C)*O(1) + O(k) ~ O(k)  for k-C+1 neurons, participating in a such worst case. 
+
+Suchwise worst-case complexity must be O(N) if I don't mess anything. //
+With the uniform distribution of neurons, average count per cell is N/(L/d)^2 which is less than 0.1 in cell, so the average complexity is C*O(N) ~ O(N)
+
+
+(Posiible microoptimisations: several shifted grids, subgrid, postpone alone-in-a-cell neurons check, write on C.)
+
+
+//Scalability? - Merge sort?
+// use complex plane/ geometry/nets/custom cells/topology?
---
+++ task2.py
@@ -0,0 +1,124 @@
+import random
+import math
+import time
+
+
+random.seed(1) # Setting random number generator seed for repeatability
+
+NUM_NEURONS = 10000
+
+NERVE_SIZE = 128000 # nanometers
+
+CONFLICT_RADIUS = 500 # nanometers
+
+
+def distance(n1, n2):
+	return math.sqrt((n1[0] - n2[0]) **2 + (n1[1] - n2[1]) **2)
+
+
+def check_for_conflicts_slow(ns, conflict_radius):
+	conflicting_idxs = {}
+	count = 0
+	for i in range(len(ns)):
+		for j in range(i+1, len(ns)):
+			if distance(ns[i], ns[j]) < conflict_radius:
+				if i not in conflicting_idxs:
+					conflicting_idxs[i] = True
+					count += 1
+				if j not in conflicting_idxs:
+					conflicting_idxs[j] = True
+					count += 1
+	##
+	# global alg_0_conflict_set 
+	# alg_0_conflict_set = set(conflicting_idxs.keys())
+	##
+	return count
+
+
+def check_for_conflicts(nerves, conflict_radius):
+	d = math.floor( conflict_radius/math.sqrt(2) )
+	cells_in_a_row = math.ceil(NERVE_SIZE/d)
+	conflicting_idxs = {}
+	count = 0
+	grid_info = {}
+	def add_conf(idx):
+		nonlocal count
+		nonlocal conflicting_idxs
+		if idx not in conflicting_idxs:
+			conflicting_idxs[ idx ] = True
+			count += 1
+
+	for i in range(len(nerves)):
+		cur_nerve = nerves[i]
+		x_grid = math.floor(cur_nerve[0]/d)
+		y_grid = math.floor(cur_nerve[1]/d)
+		if x_grid not in grid_info:
+			grid_info[x_grid] = {}
+		if y_grid not in grid_info[x_grid]:
+			grid_info[x_grid][y_grid] = []
+		elif len(grid_info[x_grid][y_grid]) > 1:
+			add_conf(i)		
+		# elif len(grid_info[x_grid][y_grid]) == 0: # empty only on creation
+		# 	print("waaat?")	
+		else:
+			j = grid_info[x_grid][y_grid][0]
+			if distance(cur_nerve, nerves[j]) < conflict_radius:
+				add_conf(i)
+				add_conf(j)
+		grid_info[x_grid][y_grid].append(i)
+
+		# Check surrounding cells that might be affected
+		x_min_affected = math.floor((cur_nerve[0] - conflict_radius)/d)
+		x_max_affected = math.floor((cur_nerve[0] + conflict_radius)/d)
+		y_min_affected = math.floor((cur_nerve[1] - conflict_radius)/d)
+		y_max_affected = math.floor((cur_nerve[1] + conflict_radius)/d)
+		for x in range(max(0, x_min_affected), min(cells_in_a_row, x_max_affected + 1)): 
+			for y in range(max(0, y_min_affected), min(cells_in_a_row, y_max_affected + 1)):
+				if not (x == x_grid and y == y_grid) and x in grid_info and y in grid_info[x]: 
+					# Alone neurons that required to be checked
+					if len(grid_info[x][y]) == 1 and grid_info[x][y][0] not in conflicting_idxs:
+						j = grid_info[x][y][0]
+						if distance(cur_nerve, nerves[j]) < conflict_radius:
+							add_conf(i)
+							add_conf(j)
+					# Checking for conflicts with neighbour cell if necessary
+					elif i not in conflicting_idxs: 
+						for j in grid_info[x][y]:
+							if distance(cur_nerve, nerves[j]) < conflict_radius:
+								add_conf(i)
+								break
+	##
+	# global alg_1_conflict_set 
+	# alg_1_conflict_set = set(conflicting_idxs.keys())
+	##
+	return count
+
+
+def gen_coord ():
+	# DO NOT MODIFY THIS FUNCTION
+	return int(random.random() * NERVE_SIZE)
+
+if __name__ == '__main__':
+	neuron_positions = [[ gen_coord () , gen_coord ()] for i in range ( NUM_NEURONS )]
+
+	n_conflicts = check_for_conflicts (neuron_positions, CONFLICT_RADIUS)
+	print(" Neurons in conflict : {}\n".format( n_conflicts ))
+	
+
+	# t_sum = 0
+	# runs_count = 1000
+	# for i in range(runs_count):
+	# 	neuron_positions = [[ gen_coord () , gen_coord ()] for i in range ( NUM_NEURONS )]
+	# 	t0 = time.time()
+	# 	check_for_conflicts (neuron_positions, CONFLICT_RADIUS)
+	# 	t_sum += time.time() - t0
+	# print(" Average execution time: {} ms".format( int(t_sum*1000/runs_count) ))
+	# ~121ms
+
+	# n_conflicts_slow = check_for_conflicts_slow(neuron_positions, CONFLICT_RADIUS)
+	## Cross-checking algorithms
+	# diff_set = alg_0_conflict_set.symmetric_difference(alg_1_conflict_set)
+	# print(" Result difference : {}\n".format( len(diff_set) ))
+	##
+
+	
